3a1f057c7f7ef71bd5d83b0b725a47705d16ffe8
alekseysafronov <polka.pisem@gmail.com>
2023-11-12 10:46:09 +0300

    Добавил файлы с компа
    С помощью командной строки не получилось. первый коммит решил пока зафигачить так

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
